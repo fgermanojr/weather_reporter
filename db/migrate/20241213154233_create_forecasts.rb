a673e699348f2acdfd52222f794f8f63a43cc5e1
fgermanojr <fgermano@earthlink.net>
2024-12-13 08:59:43 -0700

    updated migration

--- db/migrate/20241213154233_create_forecasts.rb
+++ db/migrate/20241213154233_create_forecasts.rb
@@ -1,6 +1,8 @@
 class CreateForecasts < ActiveRecord::Migration[8.0]
   def change
     create_table :forecasts do |t|
+      t.string :zipcode, index: true
+      t.text :result
       t.timestamps
     end
   end
