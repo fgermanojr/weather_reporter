f18ee9f0dd5fc5abbc2ee0db7953bc9a9c310cda
fgermanojr <fgermano@earthlink.net>
2024-12-15 18:18:34 -0700

    added validations to model spec

--- spec/models/forecast_spec.rb
+++ spec/models/forecast_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Forecast, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # test validations
+  describe ('good request') do
+    let(:forecast) { Forecast.new(zipcode: '87507') }
+
+    it 'is valid' do
+      expect(forecast).to be_valid
+    end
+  end
+
+  describe ('bad request,wrong length') do
+    let(:forecast) { Forecast.new(zipcode: '8750') }
+
+    it 'is not valid' do
+      expect(forecast.valid?).to be_falsey
+    end
+  end
+
+  describe ('bad request, not numeric') do
+    let(:forecast) { Forecast.new(zipcode: '8750A') }
+
+    it 'is not valid' do
+      expect(forecast.valid?).to be_falsey
+    end
+  end
 end
